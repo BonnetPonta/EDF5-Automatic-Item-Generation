29c549fb2747666acc3e77c0b6caeeaf5054bbce
BonnetPonta <80739002+BonnetPonta@users.noreply.github.com>
2022-06-07 19:06:57 +0900

    update file

--- Mission87.py
+++ Mission87.py
@@ -1,38 +1,51 @@
 """ mission 87 の滝近くで放置
-    後ろに下がりつつ、レイピアM4 を撃ち続ける
+    後退しつつ、レイピア を撃ち続ける
 """
 from datetime import datetime
-from time import sleep
+from time import sleep, time
 
 from keyboard import is_pressed
 from pyautogui import keyDown, keyUp, mouseDown, mouseUp
 
 
+class Colors:
+    CYAN = '\033[36m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    RESET = '\033[0m'
+
+
+def log(msg):
+    print(f"[{Colors.GREEN}{datetime.now()}{Colors.RESET}] {Colors.CYAN}{msg}{Colors.RESET}")
+
+
 def end():
-    print(f"[{datetime.now()}] The Esc key is pressed and the process is terminated.")
+    log("The Esc key is pressed and the process is terminated.")
     keyUp("s")
     mouseUp(button="left")
 
 
 def script():
-    print(f"[{datetime.now()}] START!!")
+    log("START!!")
     while True:
         if is_pressed("Esc"):
             end()
             break
         keyDown("s")
         mouseDown(button="left")
-    print(f"[{datetime.now()}] END!!")
 
 
 def main():
-    print(f"[{datetime.now()}] Processing begins after 5 seconds...")
+    log("Processing begins after 5 seconds...")
     sleep(5)
     script()
 
 
 if __name__ == "__main__":
+    start_time = time()
     try:
         main()
     except:
         end()
+    finally:
+        log(f"Time taken: {int(time()-start_time-5)} seconds")
